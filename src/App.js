1d7afea7a7139eea937593a9173d77737233987b
miihfranca <michelefdf@gmail.com>
2021-10-19 12:29:07 -0300

    arquivos da aula

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Mensagem} from "./Mensagem";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Atividade aula
+        </h1>
+        <Mensagem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mensagem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export const Mensagem = () =>{
+   return(
+        <div>
+        <h1>
+            Esperança e fé não se explicam, sentem-se.
+        </h1>
+       </div>
+    );
+   
+};
+
